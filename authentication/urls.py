34e8132a1f8dbe0e768bcf0e8ccfa02c4b601764
Jonathan Fomekong <jonathabachelard@gmail.com>
2025-01-26 15:54:01 +0100

    change litle way

--- authentication/urls.py
+++ authentication/urls.py
@@ -4,8 +4,8 @@ from rest_framework_simplejwt import views as jwt_views
 from .views import RegistrationView
 
 urlpatterns = [
-    path('login', CustomTokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('token/refresh', jwt_views.TokenRefreshView.as_view(), name='token_refresh'),
-    path('me', UserProfileView.as_view(), name='user_profile'),
+    path('login/', CustomTokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', jwt_views.TokenRefreshView.as_view(), name='token_refresh'),
+    path('me/', UserProfileView.as_view(), name='user_profile'),
     #path('register/', RegistrationView.as_view(), name='register'),
 ]
