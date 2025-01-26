d790c5d7f9092040e3983be45495a4eea9873bb0
dtl06 <tresorleroyd@gmail.com>
2025-01-26 19:17:16 +0100

    Rotes for Bill Items added

---
+++ polyclinic/api_views/bill_item_api_view.py
@@ -0,0 +1,113 @@
+from rest_framework.viewsets import ModelViewSet
+from polyclinic.models import BillItem
+from polyclinic.serializers.bill_items_serializers import BillItemSerializer
+from polyclinic.pagination import CustomPagination
+from drf_yasg.utils import swagger_auto_schema
+from drf_yasg import openapi
+from django.utils.decorators import method_decorator
+from rest_framework.permissions import IsAuthenticated
+
+auth_header_param = openapi.Parameter(
+    name="Authorization",
+    in_=openapi.IN_HEADER,
+    description="Token JWT pour l'authentification (Bearer <token>)",
+    type=openapi.TYPE_STRING,
+    required=True
+)
+
+@method_decorator(
+    name="list",
+    decorator=swagger_auto_schema(
+        operation_summary="Lister les objets",
+        operation_description=(
+            "Cette route retourne une liste paginée de tous les objets du modèle. "
+            "L'authentification est requise pour accéder à cette ressource."
+        ),
+        manual_parameters=[auth_header_param],
+        tags = ["BillItems"]
+    )
+)
+@method_decorator(
+    name="retrieve",
+    decorator=swagger_auto_schema(
+        operation_summary="Récupérer un objet",
+        operation_description=(
+            "Cette route retourne les détails d'un objet spécifique en fonction de son ID. "
+            "L'authentification est requise pour accéder à cette ressource."
+        ),
+        manual_parameters=[auth_header_param],
+        tags = ["BillItems"]
+    )
+)
+@method_decorator(
+    name="create",
+    decorator=swagger_auto_schema(
+        operation_summary="Créer un nouvel objet",
+        operation_description=(
+            "Cette route permet de créer un nouvel objet. "
+            "Les données doivent être envoyées dans le corps de la requête. "
+            "L'authentification est requise pour accéder à cette ressource."
+        ),
+        manual_parameters=[auth_header_param],
+        tags = ["BillItems"]
+    )
+)
+@method_decorator(
+    name="update",
+    decorator=swagger_auto_schema(
+        operation_summary="Mettre à jour un objet",
+        operation_description=(
+            "Cette route permet de mettre à jour complètement un objet existant en fonction de son ID. "
+            "Les données doivent être envoyées dans le corps de la requête. "
+            "L'authentification est requise pour accéder à cette ressource."
+        ),
+        manual_parameters=[auth_header_param],
+        tags = ["BillItems"]
+    )
+)
+@method_decorator(
+    name="partial_update",
+    decorator=swagger_auto_schema(
+        operation_summary="Mise à jour partielle d'un objet",
+        operation_description=(
+            "Cette route permet de mettre à jour partiellement un objet existant en fonction de son ID. "
+            "Les données doivent être envoyées dans le corps de la requête. "
+            "L'authentification est requise pour accéder à cette ressource."
+        ),
+        manual_parameters=[auth_header_param],
+        tags = ["BillItems"]
+    )
+)
+@method_decorator(
+    name="destroy",
+    decorator=swagger_auto_schema(
+        operation_summary="Supprimer un objet",
+        operation_description=(
+            "Cette route permet de supprimer un objet existant en fonction de son ID. "
+            "L'authentification est requise pour accéder à cette ressource."
+        ),
+        manual_parameters=[auth_header_param],
+        tags = ["BillItems"]
+    )
+)
+class BillItemViewSet(ModelViewSet):
+
+    permission_classes = [IsAuthenticated]
+    pagination_class = CustomPagination
+
+    def get_queryset(self):
+        queryset = BillItem.objects.all()
+        return queryset
+
+    def get_serializer_class(self):
+        return BillItemSerializer
+
+    def perform_create(self, serializer):
+        if 'id' in serializer.validated_data:
+            serializer.validated_data.pop('id')
+        serializer.save()
+
+    def perform_update(self, serializer):
+        if 'id' in serializer.validated_data:
+            serializer.validated_data.pop('id')
+        serializer.save()
