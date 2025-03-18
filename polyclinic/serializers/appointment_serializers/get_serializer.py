d3361014b080570607ae0b3c2e44c30dd72be174
Jonathan Fomekong <jonathabachelard@gmail.com>
2025-03-18 11:32:06 +0100

    fix appointment serializer

--- polyclinic/serializers/appointment_serializers/get_serializer.py
+++ polyclinic/serializers/appointment_serializers/get_serializer.py
@@ -1,8 +1,11 @@
 from rest_framework.exceptions import ValidationError
 from rest_framework import serializers
 from polyclinic.models import Appointment
+from polyclinic.serializers.patient_serializers import PatientSerializer
+
 
 class AppointmentSerializer(serializers.ModelSerializer):
+    idPatient = PatientSerializer(read_only=True)
 
     class Meta:
         model = Appointment
